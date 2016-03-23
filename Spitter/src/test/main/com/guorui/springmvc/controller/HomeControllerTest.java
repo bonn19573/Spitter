157e1f6d80eb6d4fd2629910c513f9b6e6ef5233
bonn19573 <bonn19573@hotmail.com>
2016-03-23 21:17:35 +0800

    use MockMvc to test HomeController

--- Spitter/src/test/main/com/guorui/springmvc/controller/HomeControllerTest.java
+++ Spitter/src/test/main/com/guorui/springmvc/controller/HomeControllerTest.java
@@ -1,14 +1,19 @@
 package com.guorui.springmvc.controller;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
 
 public class HomeControllerTest {
 
 	@Test
-	public void testHome() {
+	public void testHome() throws Exception {
 		HomeController controller = new HomeController();
+		MockMvc mockMvc = standaloneSetup(controller).build();
+		mockMvc.perform(get("/")).andExpect(view().name("home"));
 		String home = controller.home();
 		assertEquals("home", home);
 	}
